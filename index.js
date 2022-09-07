6e60d2fc8b092592d3b9b3a64d5c87f6fb0056b3
Luca Ferreyra <ferreyraluca2003@gmail.com>
2022-09-07 20:54:48 -0300

    1er entrega proyecto final

--- index.js
+++ index.js
@@ -1,56 +1,64 @@
-let productoSeleccionado = parseInt(prompt("Ingrese el producto que desea comprar: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"))
-let seguir = true
-let totalCompra = 0
-let decision 
-let carrito = []
-let productos = []
-
-const amdRyzen3 = {
-    nombre: "AMD RYZEN 3",
-    precio: 3000,
-}
-productos.push(amdRyzen3)
-const amdRyzen5 = {
-    nombre: "AMD RYZEN 5",
-    precio: 5000
-}
-productos.push(amdRyzen5)
-const amdRyzen7 = {
-    nombre: "AMD RYZEN 7",
-    precio: 9000
-}
-productos.push(amdRyzen7)
-const amdRyzen9 = {
-    nombre: "AMD RYZEN 9",
-    precio: 15000
-}
-productos.push(amdRyzen9)
-
-while (seguir===true){
-    if (productoSeleccionado === 1){
-        carrito.push(productos[0])
-    } else if (productoSeleccionado === 2){
-        carrito.push(productos[1])
-    } else if (productoSeleccionado === 3){
-        carrito.push(productos[2])
-    } else if (productoSeleccionado === 4){
-        carrito.push(productos[3])
-    } else{
-        productoSeleccionado = parseInt(prompt("Ingrese un producto correcto: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"));
-        continue
+let elegirProducto = parseInt(
+    prompt(
+        "Ingrese el producto que desea comprar: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"
+    )
+);
+let seguir = true;
+let totalCompra = 0;
+let productos = [];
+let decision;
+const iva = 1.21;
+class producto {
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
     }
-    
-    decision = parseInt(prompt("Deseas seguir comprando? 1.SI - 2.NO"))
-    if (decision === 1){
-        productoSeleccionado = parseInt(prompt("Ingrese un producto correcto: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"));
-    } else if(decision === 2){
-        seguir = false
+
+    sumaImpuestos () {
+        this.precio = this.precio * iva
     }
 }
 
+let productos1 = new producto(1, "Amd Ryzen 3", 24500);
+productos.push(productos1);
+let productos2 = new producto(2, "Amd Ryzen 5", 45900);
+productos.push(productos2);
+let productos3 = new producto(3, "Amd Ryzen 7", 82950);
+productos.push(productos3);
+let productos4 = new producto(4, "Amd Ryzen 9", 127500);
+productos.push(productos4);
 
-for (const elemento of carrito) {
-    totalCompra = totalCompra + elemento.precio;
+productos1.sumaImpuestos ();
+productos2.sumaImpuestos ();
+productos3.sumaImpuestos ();
+productos4.sumaImpuestos ();
+
+while (seguir === true) {
+    let productoSeleccionado = productos.find(
+        (prod) => prod.id === elegirProducto
+    );
+    if (productoSeleccionado) {
+        totalCompra = totalCompra + productoSeleccionado.precio;
+    } else {
+        elegirProducto = parseInt(
+            prompt(
+                "Ingrese un producto correcto: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"
+            )
+        );
+        continue;
+    }
+
+    decision = parseInt(prompt("Deseas seguir comprando? 1.Si - 2.No"));
+    if (decision === 1) {
+        elegirProducto = parseInt(
+            prompt(
+                "Ingrese el producto que desea comprar: 1.AMD RYZEN 3 - 2.AMD RYZEN 5 - 3.AMD RYZEN 7 - 4.AMD RYZEN 9"
+            )
+        );
+    } else if (decision === 2) {
+        seguir = false;
+    }
 }
 
-    alert ("El valor total es de: " + totalCompra)
+alert("El total a pagar es de: " + totalCompra);
