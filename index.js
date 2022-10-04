14556b95875389dbe346499bac53a5452df7d178
Luca Ferreyra <ferreyraluca2003@gmail.com>
2022-10-04 18:04:30 -0300

    Desafío: "Fetch en tu proyecto"

--- index.js
+++ index.js
@@ -1,116 +1,30 @@
-const contenedorCard = document.getElementById("containerCard")
-const contadorCarrito = document.getElementById("counterCard")
-const contenedorCarrito = document.getElementById('carrito-contenedor')
-const precioTotal = document.getElementById("precioTotal")
-
-let carrito = []
-
 document.addEventListener('DOMContentLoaded', () => {
-    if (localStorage.getItem('carrito')){
-        carrito = JSON.parse(localStorage.getItem('carrito'))
-        actualizarCarrito()
-    }
+    fetchData();
 })
 
-//mostrar cards
-productos.forEach((producto) => {
-    const div = document.createElement('div')
-    div.innerHTML =
-        `<div class="card" style="width: 18rem;">
-    <img src= ${producto.img} class="img card-img-top" alt="...">
-    <div class="card-body">
-        <h5 class="card-title">${producto.nombre}</h5>
-        <p class="card-text">$${producto.precio}</p>
-        <button id=${producto.id} class="btn btn-primary">Agregar al carrito</button>
-    </div>
-</div>`
-
-    contenedorCard.appendChild(div)
-
-    let boton = document.getElementById(producto.id)
-    boton.addEventListener('click', () => {
-        toastJs()
-        agregarAlCarrito(producto.id)
-    })
-
-})
-
-//funciones toastify´s
-
-function toastJs (){
-    Toastify({
-        text: "Agregado al carrito",
-        duration: 3000,
-        gravity: "bottom",
-        style: {
-            background: "#373b44",
-            background: "-webkit-linear-gradient(to right, #373b44, #4286f4)",
-            background: "linear-gradient(to right, #373b44, #4286f4)"
-        }
-    }).showToast()
-}
-
-function toastJsEliminar (){
-    Toastify({
-        text: "Eliminado del carrito",
-        duration: 3000,
-        gravity: "bottom",
-        style: {
-            background: "#373b44",
-            background: "-webkit-linear-gradient(to right, #373b44, #4286f4)",
-            background: "linear-gradient(to right, #373b44, #4286f4)"
-        }
-    }).showToast()
-}
-
-
-const agregarAlCarrito = (prodId) => {
-
-    const existe = carrito.some (prod => prod.id === prodId)
-    if(existe){
-        const prod = carrito.map (prod => {
-            if (prod.id === prodId){
-                //aplico operador de incremento
-                prod.cantidad++
-            }
-        })
-    } else {
-        const item = productos.find((prod) => prod.id === prodId)
-        carrito.push(item)
+const fetchData = async () => {
+    try {
+        const res = await fetch("https://rickandmortyapi.com/api/character")
+        const data = await res.json()
+        pintarCards(data)
+    } catch (error) {       
     }
-    actualizarCarrito()
-    
 }
 
-const eliminarDelCarrito = (prodId) => {
-    const item = carrito.find((producto) => producto.id === prodId)
-    const indice = carrito.indexOf(item)
-    carrito.splice(indice, 1)
-    toastJsEliminar ()
-    actualizarCarrito()
-}
+const pintarCards = (data) => {
+    const card = document.querySelector('#card-dinamicas');
+    const templateCard = document.querySelector('#template-card').content;
+    const fragment = document.createDocumentFragment()
 
-const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = ""
+    data.results.forEach(element => {
+        const clone = templateCard.cloneNode(true)
+        clone.querySelector('h5').textContent = element.name
+        clone.querySelector('p').textContent = element.species
+        clone.querySelector('img').setAttribute("src", element.image)
 
-    carrito.forEach((producto) => {
-        const div = document.createElement('div')
-        div.classList.add('producto')
-        div.innerHTML = `
-        <p>Producto: ${producto.nombre}</p>
-        <p>Precio: $${producto.precio}</p>
-        <p>Cantidad: ${producto.cantidad}</p>
-        <button onclick = "eliminarDelCarrito(${producto.id})" class="boton-eliminar"><i class="icon fas fa-trash-alt"></i></button>
-        `
-        contenedorCarrito.appendChild (div)
         
-        localStorage.setItem('carrito', JSON.stringify(carrito))
-    })
+        fragment.appendChild(clone)
+    });
 
-    contadorCarrito.innerText = carrito.length
-    precioTotal.innerText = carrito.reduce ((acc, prod) => acc + prod.precio*prod.cantidad, 0)
+    card.append(fragment)
 }
-
-
-
-
--- productos.js
+++
@@ -1,6 +0,0 @@
-let productos = [
-    {id: 1, nombre: "Ryzen 3", cantidad: 1, precio: 20000, img: 'img/ryzen3.jpg'},
-    {id: 2, nombre: "Ryzen 5", cantidad: 1, precio: 40000, img: 'img/ryzen5.jpg'},
-    {id: 3, nombre: "Ryzen 7", cantidad: 1, precio: 60000, img: 'img/ryzen7.jpg'},
-    {id: 4, nombre: "Ryzen 9", cantidad: 1, precio: 80000, img: 'img/ryzen9.jpg'},
-]
